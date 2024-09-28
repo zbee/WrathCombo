35396800ea3f82d4b22cd37d58bb16d4fc5e203c
Tartarga <109563717+Tartarga@users.noreply.github.com>
2024-09-28 12:53:11 -0500

    Config updated to Switch statement

--- XIVSlothCombo/Combos/PvE/SCH/SCH_Config.cs
+++ XIVSlothCombo/Combos/PvE/SCH/SCH_Config.cs
@@ -59,146 +59,144 @@ namespace XIVSlothCombo.Combos.PvE
 
             internal static void Draw(CustomComboPreset preset)
             {
-                if (preset is CustomComboPreset.SCH_DPS)
+                switch (preset)
                 {
-                    DrawAdditionalBoolChoice(SCH_ST_DPS_Adv, "Advanced Action Options", "Change how actions are handled", isConditionalChoice: true);
+                    case CustomComboPreset.SCH_DPS:
+                        DrawAdditionalBoolChoice(SCH_ST_DPS_Adv, "Advanced Action Options", "Change how actions are handled", isConditionalChoice: true);
+                        if (SCH_ST_DPS_Adv)
+                        {
+                            ImGui.Indent(); ImGui.Spacing();
+                            DrawHorizontalMultiChoice(SCH_ST_DPS_Adv_Actions, "On Ruin/Broils", "Apply options to Ruin and all Broils.", 3, 0);
+                            DrawHorizontalMultiChoice(SCH_ST_DPS_Adv_Actions, "On Bio/Bio II/Biolysis", "Apply options to Bio and Biolysis.", 3, 1);
+                            DrawHorizontalMultiChoice(SCH_ST_DPS_Adv_Actions, "On Ruin II", "Apply options to Ruin II.", 3, 2);
+                            ImGui.Unindent();
+                        }
+                        break;
 
-                    if (SCH_ST_DPS_Adv)
-                    {
-                        ImGui.Indent(); ImGui.Spacing();
-                        DrawHorizontalMultiChoice(SCH_ST_DPS_Adv_Actions, "On Ruin/Broils", "Apply options to Ruin and all Broils.", 3, 0);
-                        DrawHorizontalMultiChoice(SCH_ST_DPS_Adv_Actions, "On Bio/Bio II/Biolysis", "Apply options to Bio and Biolysis.", 3, 1);
-                        DrawHorizontalMultiChoice(SCH_ST_DPS_Adv_Actions, "On Ruin II", "Apply options to Ruin II.", 3, 2);
-                        ImGui.Unindent();
-                    }
+                    case CustomComboPreset.SCH_DPS_Lucid:
+                        DrawSliderInt(4000, 9500, SCH_ST_DPS_LucidOption, "MP Threshold", 150, Hundreds);
+                        break;
+
+                    case CustomComboPreset.SCH_DPS_Bio:
+                        DrawSliderInt(0, 100, SCH_ST_DPS_BioOption, "Stop using at Enemy HP%. Set to Zero to disable this check.");
+
+                        DrawAdditionalBoolChoice(SCH_ST_DPS_Bio_Adv, "Advanced Options", "", isConditionalChoice: true);
+                        if (SCH_ST_DPS_Bio_Adv)
+                        {
+                            ImGui.Indent();
+                            DrawRoundedSliderFloat(0, 4, SCH_ST_DPS_Bio_Threshold, "Seconds remaining before reapplying the DoT. Set to Zero to disable this check.", digits: 1);
+                            ImGui.Unindent();
+                        }
+                        break;
+
+                    case CustomComboPreset.SCH_DPS_ChainStrat:
+                        DrawSliderInt(0, 100, SCH_ST_DPS_ChainStratagemOption, "Stop using at Enemy HP%. Set to Zero to disable this check.");
+                        break;
+
+                    case CustomComboPreset.SCH_DPS_EnergyDrain:
+                        DrawAdditionalBoolChoice(SCH_ST_DPS_EnergyDrain_Adv, "Advanced Options", "", isConditionalChoice: true);
+                        if (SCH_ST_DPS_EnergyDrain_Adv)
+                        {
+                            ImGui.Indent();
+                            DrawRoundedSliderFloat(0, 60, SCH_ST_DPS_EnergyDrain, "Aetherflow remaining cooldown:", digits: 1);
+                            ImGui.Unindent();
+                        }
+                        break;
+
+                    case CustomComboPreset.SCH_ST_Heal:
+                        DrawAdditionalBoolChoice(SCH_ST_Heal_Adv, "Advanced Options", "", isConditionalChoice: true);
+                        if (SCH_ST_Heal_Adv)
+                        {
+                            ImGui.Indent();
+                            DrawAdditionalBoolChoice(SCH_ST_Heal_UIMouseOver,
+                                "Party UI Mouseover Checking",
+                                "Check party member's HP & Debuffs by using mouseover on the party list.\n" +
+                                "To be used in conjunction with Redirect/Reaction/etc");
+                            ImGui.Unindent();
+                        }
+                        break;
+
+                    case CustomComboPreset.SCH_ST_Heal_Lucid:
+                        DrawSliderInt(4000, 9500, SCH_ST_Heal_LucidOption, "MP Threshold", 150, Hundreds);
+                        break;
+
+                    case CustomComboPreset.SCH_ST_Heal_Adloquium:
+                        DrawSliderInt(0, 100, SCH_ST_Heal_AdloquiumOption, "Use Adloquium on targets at or below HP % even if they have Galvanize\n0 = Only ever use Adloquium on targets without Galvanize\n100 = Always use Adloquium");
+                        break;
+
+                    case CustomComboPreset.SCH_ST_Heal_Lustrate:
+                        DrawSliderInt(0, 100, SCH_ST_Heal_LustrateOption, "Start using when below HP %. Set to 100 to disable this check");
+                        DrawPriorityInput(SCH_ST_Heals_Priority, 3, 0, $"{Lustrate.ActionName()} Priority: ");
+                        break;
+
+                    case CustomComboPreset.SCH_ST_Heal_Excogitation:
+                        DrawSliderInt(0, 100, SCH_ST_Heal_ExcogitationOption, "Start using when below HP %. Set to 100 to disable this check");
+                        DrawPriorityInput(SCH_ST_Heals_Priority, 3, 1, $"{Excogitation.ActionName()} Priority: ");
+                        break;
+
+                    case CustomComboPreset.SCH_ST_Heal_Protraction:
+                        DrawSliderInt(0, 100, SCH_ST_Heal_ProtractionOption, "Start using when below HP %. Set to 100 to disable this check");
+                        DrawPriorityInput(SCH_ST_Heals_Priority, 3, 2, $"{Protraction.ActionName()} Priority: ");
+                        break;
+
+                    case CustomComboPreset.SCH_ST_Heal_Esuna:
+                        DrawSliderInt(0, 100, SCH_ST_Heal_EsunaOption, "Stop using when below HP %. Set to Zero to disable this check");
+                        break;
+
+                    case CustomComboPreset.SCH_AoE_Lucid:
+                        DrawSliderInt(4000, 9500, SCH_AoE_LucidOption, "MP Threshold", 150, Hundreds);
+                        break;
+
+                    case CustomComboPreset.SCH_AoE_Heal_Lucid:
+                        DrawSliderInt(4000, 9500, SCH_AoE_Heal_LucidOption, "MP Threshold", 150, Hundreds);
+                        break;
+
+                    case CustomComboPreset.SCH_DeploymentTactics:
+                        DrawAdditionalBoolChoice(SCH_DeploymentTactics_Adv, "Advanced Options", "", isConditionalChoice: true);
+                        if (SCH_DeploymentTactics_Adv)
+                        {
+                            ImGui.Indent();
+                            DrawAdditionalBoolChoice(SCH_DeploymentTactics_UIMouseOver,
+                                "Party UI Mouseover Checking",
+                                "Check party member's HP & Debuffs by using mouseover on the party list.\n" +
+                                "To be used in conjunction with Redirect/Reaction/etc");
+                            ImGui.Unindent();
+                        }
+                        break;
+
+                    case CustomComboPreset.SCH_Aetherflow:
+                        DrawRadioButton(SCH_Aetherflow_Display, "Show Aetherflow On Energy Drain Only", "", 0);
+                        DrawRadioButton(SCH_Aetherflow_Display, "Show Aetherflow On All Aetherflow Skills", "", 1);
+                        break;
+
+                    case CustomComboPreset.SCH_Aetherflow_Recite:
+                        DrawAdditionalBoolChoice(SCH_Aetherflow_Recite_Excog, "On Excogitation", "", isConditionalChoice: true);
+                        if (SCH_Aetherflow_Recite_Excog)
+                        {
+                            ImGui.Indent(); ImGui.Spacing();
+                            DrawRadioButton(SCH_Aetherflow_Recite_ExcogMode, "Only when out of Aetherflow Stacks", "", 0);
+                            DrawRadioButton(SCH_Aetherflow_Recite_ExcogMode, "Always when available", "", 1);
+                            ImGui.Unindent();
+                        }
+
+                        DrawAdditionalBoolChoice(SCH_Aetherflow_Recite_Indom, "On Indominability", "", isConditionalChoice: true);
+                        if (SCH_Aetherflow_Recite_Indom)
+                        {
+                            ImGui.Indent(); ImGui.Spacing();
+                            DrawRadioButton(SCH_Aetherflow_Recite_IndomMode, "Only when out of Aetherflow Stacks", "", 0);
+                            DrawRadioButton(SCH_Aetherflow_Recite_IndomMode, "Always when available", "", 1);
+                            ImGui.Unindent();
+                        }
+                        break;
+
+                    case CustomComboPreset.SCH_Recitation:
+                        DrawRadioButton(SCH_Recitation_Mode, "Adloquium", "", 0);
+                        DrawRadioButton(SCH_Recitation_Mode, "Succor", "", 1);
+                        DrawRadioButton(SCH_Recitation_Mode, "Indomitability", "", 2);
+                        DrawRadioButton(SCH_Recitation_Mode, "Excogitation", "", 3);
+                        break;
                 }
 
-                if (preset is CustomComboPreset.SCH_DPS_Lucid)
-                    DrawSliderInt(4000, 9500, SCH_ST_DPS_LucidOption, "MP Threshold", 150, Hundreds);
-
-                if (preset is CustomComboPreset.SCH_DPS_Bio)
-                {
-                    DrawSliderInt(0, 100, SCH_ST_DPS_BioOption, "Stop using at Enemy HP%. Set to Zero to disable this check.");
-
-                    DrawAdditionalBoolChoice(SCH_ST_DPS_Bio_Adv, "Advanced Options", "", isConditionalChoice: true);
-                    if (SCH_ST_DPS_Bio_Adv)
-                    {
-                        ImGui.Indent();
-                        DrawRoundedSliderFloat(0, 4, SCH_ST_DPS_Bio_Threshold, "Seconds remaining before reapplying the DoT. Set to Zero to disable this check.", digits: 1);
-                        ImGui.Unindent();
-                    }
-                }
-
-                if (preset is CustomComboPreset.SCH_DPS_ChainStrat)
-                    DrawSliderInt(0, 100, SCH_ST_DPS_ChainStratagemOption, "Stop using at Enemy HP%. Set to Zero to disable this check.");
-
-                if (preset is CustomComboPreset.SCH_DPS_EnergyDrain)
-                {
-                    DrawAdditionalBoolChoice(SCH_ST_DPS_EnergyDrain_Adv, "Advanced Options", "", isConditionalChoice: true);
-                    if (SCH_ST_DPS_EnergyDrain_Adv)
-                    {
-                        ImGui.Indent();
-                        DrawRoundedSliderFloat(0, 60, SCH_ST_DPS_EnergyDrain, "Aetherflow remaining cooldown:", digits: 1);
-                        ImGui.Unindent();
-                    }
-                }
-
-                if (preset is CustomComboPreset.SCH_ST_Heal)
-                {
-                    DrawAdditionalBoolChoice(SCH_ST_Heal_Adv, "Advanced Options", "", isConditionalChoice: true);
-                    if (SCH_ST_Heal_Adv)
-                    {
-                        ImGui.Indent();
-                        DrawAdditionalBoolChoice(SCH_ST_Heal_UIMouseOver,
-                            "Party UI Mouseover Checking",
-                            "Check party member's HP & Debuffs by using mouseover on the party list.\n" +
-                            "To be used in conjunction with Redirect/Reaction/etc");
-                        ImGui.Unindent();
-                    }
-                }
-
-                if (preset is CustomComboPreset.SCH_ST_Heal_Lucid)
-                    DrawSliderInt(4000, 9500, SCH_ST_Heal_LucidOption, "MP Threshold", 150, Hundreds);
-
-                if (preset is CustomComboPreset.SCH_ST_Heal_Adloquium)
-                    DrawSliderInt(0, 100, SCH_ST_Heal_AdloquiumOption, "Use Adloquium on targets at or below HP % even if they have Galvanize\n0 = Only ever use Adloquium on targets without Galvanize\n100 = Always use Adloquium");
-
-                if (preset is CustomComboPreset.SCH_ST_Heal_Lustrate)
-                {
-                    DrawSliderInt(0, 100, SCH_ST_Heal_LustrateOption, "Start using when below HP %. Set to 100 to disable this check");
-                    DrawPriorityInput(SCH_ST_Heals_Priority, 3, 0, $"{Lustrate.ActionName()} Priority: ");
-                }
-
-                if (preset is CustomComboPreset.SCH_ST_Heal_Excogitation)
-                {
-                    DrawSliderInt(0, 100, SCH_ST_Heal_ExcogitationOption, "Start using when below HP %. Set to 100 to disable this check");
-                    DrawPriorityInput(SCH_ST_Heals_Priority, 3, 1, $"{Excogitation.ActionName()} Priority: ");
-                }
-
-                if (preset is CustomComboPreset.SCH_ST_Heal_Protraction)
-                {
-                    DrawSliderInt(0, 100, SCH_ST_Heal_ProtractionOption, "Start using when below HP %. Set to 100 to disable this check");
-                    DrawPriorityInput(SCH_ST_Heals_Priority, 3, 2, $"{Protraction.ActionName()} Priority: ");
-                }
-
-                if (preset is CustomComboPreset.SCH_ST_Heal_Esuna)
-                    DrawSliderInt(0, 100, SCH_ST_Heal_EsunaOption, "Stop using when below HP %. Set to Zero to disable this check");
-
-                if (preset is CustomComboPreset.SCH_AoE_Lucid)
-                    DrawSliderInt(4000, 9500, SCH_AoE_LucidOption, "MP Threshold", 150, Hundreds);
-
-                if (preset is CustomComboPreset.SCH_AoE_Heal_Lucid)
-                    DrawSliderInt(4000, 9500, SCH_AoE_Heal_LucidOption, "MP Threshold", 150, Hundreds);
-
-                if (preset is CustomComboPreset.SCH_DeploymentTactics)
-                {
-                    DrawAdditionalBoolChoice(SCH_DeploymentTactics_Adv, "Advanced Options", "", isConditionalChoice: true);
-                    if (SCH_DeploymentTactics_Adv)
-                    {
-                        ImGui.Indent();
-                        DrawAdditionalBoolChoice(SCH_DeploymentTactics_UIMouseOver,
-                            "Party UI Mouseover Checking",
-                            "Check party member's HP & Debuffs by using mouseover on the party list.\n" +
-                            "To be used in conjunction with Redirect/Reaction/etc");
-                        ImGui.Unindent();
-                    }
-                }
-
-                if (preset is CustomComboPreset.SCH_Aetherflow)
-                {
-                    DrawRadioButton(SCH_Aetherflow_Display, "Show Aetherflow On Energy Drain Only", "", 0);
-                    DrawRadioButton(SCH_Aetherflow_Display, "Show Aetherflow On All Aetherflow Skills", "", 1);
-                }
-
-                if (preset is CustomComboPreset.SCH_Aetherflow_Recite)
-                {
-                    DrawAdditionalBoolChoice(SCH_Aetherflow_Recite_Excog, "On Excogitation", "", isConditionalChoice: true);
-                    if (SCH_Aetherflow_Recite_Excog)
-                    {
-                        ImGui.Indent(); ImGui.Spacing();
-                        DrawRadioButton(SCH_Aetherflow_Recite_ExcogMode, "Only when out of Aetherflow Stacks", "", 0);
-                        DrawRadioButton(SCH_Aetherflow_Recite_ExcogMode, "Always when available", "", 1);
-                        ImGui.Unindent();
-                    }
-
-                    DrawAdditionalBoolChoice(SCH_Aetherflow_Recite_Indom, "On Indominability", "", isConditionalChoice: true);
-                    if (SCH_Aetherflow_Recite_Indom)
-                    {
-                        ImGui.Indent(); ImGui.Spacing();
-                        DrawRadioButton(SCH_Aetherflow_Recite_IndomMode, "Only when out of Aetherflow Stacks", "", 0);
-                        DrawRadioButton(SCH_Aetherflow_Recite_IndomMode, "Always when available", "", 1);
-                        ImGui.Unindent();
-                    }
-                }
-
-                if (preset is CustomComboPreset.SCH_Recitation)
-                {
-                    DrawRadioButton(SCH_Recitation_Mode, "Adloquium", "", 0);
-                    DrawRadioButton(SCH_Recitation_Mode, "Succor", "", 1);
-                    DrawRadioButton(SCH_Recitation_Mode, "Indomitability", "", 2);
-                    DrawRadioButton(SCH_Recitation_Mode, "Excogitation", "", 3);
-                }
-                
             }
         }
     }
