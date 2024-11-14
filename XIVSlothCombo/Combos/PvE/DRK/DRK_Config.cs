44829960915828923e0a13e3ba8fa7b93f79291e
Ethan Henderson <ethan@zbee.codes>
2024-11-13 19:37:34 -0700

    Reformat config layout

--- XIVSlothCombo/Combos/PvE/DRK/DRK_Config.cs
+++ XIVSlothCombo/Combos/PvE/DRK/DRK_Config.cs
@@ -16,43 +16,47 @@ internal partial class DRK
 
         internal static void Draw(CustomComboPreset preset)
         {
-            #region DARK KNIGHT
-
             switch (preset)
             {
                 case CustomComboPreset.DRK_ST_ManaSpenderPooling:
                     UserConfig.DrawSliderInt(0, 3000, DRK_ST_ManaSpenderPooling,
-                        "How much MP to save (0 = Use All)", 150, SliderIncrements.Thousands);
+                        "How much MP to reserve " +
+                        "(0 = Use All)",
+                        itemWidth: 150f, sliderIncrement:SliderIncrements.Thousands);
 
                     break;
 
                 case CustomComboPreset.DRK_ST_CDs_LivingShadow:
                     UserConfig.DrawSliderInt(0, 30, DRK_ST_LivingDeadThreshold,
-                        "Stop Using When Target HP% is at or Below (Set to 0 to Disable This Check)", 150,
-                        SliderIncrements.Fives);
+                        "Stop Using When Target HP% is at or Below " +
+                        "(0 = Use Always)",
+                        itemWidth: 150f, sliderIncrement:SliderIncrements.Fives);
 
                     break;
 
                 case CustomComboPreset.DRK_AoE_CDs_LivingShadow:
                     UserConfig.DrawSliderInt(0, 60, DRK_AoE_LivingDeadThreshold,
-                        "Stop Using When Target HP% is at or Below (Set to 0 to Disable This Check)", 150,
-                        SliderIncrements.Fives);
+                        "Stop Using When Target HP% is at or Below " +
+                        "(0 = Use Always)",
+                        itemWidth: 150f, sliderIncrement:SliderIncrements.Fives);
 
                     break;
 
                 case CustomComboPreset.DRKPvP_Burst:
                     UserConfig.DrawSliderInt(1, 100, DRKPvP.Config.ShadowbringerThreshold,
-                        "HP% to be at or above to use Shadowbringer");
+                        "HP% to be at or Above to use " +
+                        "(0 = Use Always)",
+                        itemWidth: 150f, sliderIncrement:SliderIncrements.Fives);
 
                     break;
 
                 case CustomComboPreset.DRK_Variant_Cure:
-                    UserConfig.DrawSliderInt(1, 100, DRK_VariantCure, "HP% to be at or under", 200);
+                    UserConfig.DrawSliderInt(1, 100, DRK_VariantCure,
+                        "HP% to be at or Below",
+                        itemWidth: 200, sliderIncrement:SliderIncrements.Fives);
 
                     break;
             }
-
-            #endregion
         }
     }
 }
