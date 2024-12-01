1bdd4e63bd99ad63d2deb3903da55598d2e56ef7
edewen <edewen@gmail.com>
2024-11-30 20:57:11 -0500

    zbee indent change

--- WrathCombo/Combos/PvP/PVPCommon.cs
+++ WrathCombo/Combos/PvP/PVPCommon.cs
@@ -115,9 +115,9 @@ namespace WrathCombo.Combos.PvP
                     {
                         if (LocalPlayer.CurrentMp >= 2500 && LocalPlayer.CurrentHp <= LocalPlayer.MaxHp - 15000) 
                             return Recuperate;
-                            return Guard;
+                        return Guard;
                     }
-                    else return OriginalHook(11);
+                    return OriginalHook(11);
                 }
 
                 if (Execute() &&
