398b862699bba291f1801455898e0c981cf6b7ca
Kage <95875972+Kagekazu@users.noreply.github.com>
2024-12-08 13:57:11 +0100

    Serpents Tail standalone option

--- WrathCombo/Combos/CustomComboPreset.cs
+++ WrathCombo/Combos/CustomComboPreset.cs
@@ -4865,6 +4865,10 @@ namespace WrathCombo.Combos
         [CustomComboInfo("Legacy Buttons", "Replaces Generations with the Legacys.", VPR.JobID)]
         VPR_Legacies = 30209,
 
+        [ReplaceSkill(VPR.SteelFangs, VPR.ReavingFangs, VPR.SteelMaw, VPR.ReavingMaw)]
+        [CustomComboInfo("Serpents Tail", "Replaces basic combo with Death Rattle or Last Lash when applicable.", VPR.JobID)]
+        VPR_SerpentsTail = 30210,
+
         #endregion
 
         #region WARRIOR
