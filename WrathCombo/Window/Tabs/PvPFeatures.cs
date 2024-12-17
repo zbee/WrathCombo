78b81dcd195f37b2c667c83060d9b6e6d9d76137
Ethan Henderson <ethan@zbee.codes>
2024-12-16 20:12:26 -0700

    Jobs will now show if they are IPC-controlled

--- WrathCombo/Window/Tabs/PvPFeatures.cs
+++ WrathCombo/Window/Tabs/PvPFeatures.cs
@@ -67,6 +67,14 @@ namespace WrathCombo.Window.Tabs
                                 ImGui.SameLine(indentwidth2);
                             }
                             ImGui.Text($"{header} {(disabled ? "(Disabled due to update)" : "")}");
+
+                            if (!string.IsNullOrEmpty(abbreviation) &&
+                                P.IPC.UIHelper.JobControlled(id) is not null)
+                            {
+                                ImGui.SameLine();
+                                P.IPC.UIHelper
+                                    .ShowIPCControlledIndicatorIfNeeded(id);
+                            }
                         }
                     }
                 }
